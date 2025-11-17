1fb5f33fbcc073151124d65810c7a8ae3c852db1
angelina <masteniangelina17@gmail.com>
2025-11-17 09:59:52 -0800

    some fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
+from typing import Union
 
 class Settings(BaseSettings):
     app_name: str = "FastAPI Shop"
     debug: bool=True
     database_url: str = "sqlite:///./shop.db"
-    cors_origins: list = [
+    cors_origins: Union[list[str], str] = [
         "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:5173",

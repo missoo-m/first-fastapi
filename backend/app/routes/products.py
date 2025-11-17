a1ef6e5b3d2aed71ef1a2d4a099506c2ce61b34d
angelina <masteniangelina17@gmail.com>
2025-11-17 08:52:35 -0800

    add frontend (vue.js)

--- backend/app/routes/products.py
+++ backend/app/routes/products.py
@@ -15,7 +15,7 @@ def get_products(db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_all_products()
 
-@router.get("/{product_id}",response_model=List[ProductResponse], status_code = status.HTTP_200_OK)
+@router.get("/{product_id}",response_model= ProductResponse, status_code = status.HTTP_200_OK)
 def get_product(product_id : int , db: Session = Depends(get_db)):
     service = ProductService(db)
     return service.get_product_by_id(product_id)
